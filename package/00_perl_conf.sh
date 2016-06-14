71b4a91b21c5fa4c958ac6417c6f7b73ad800163
Yixf(Yi Xianfu) <yixfgit@gmail.com>
2016-06-14 12:18:44 +0800

    Add the fastest mirror for CPAN

--- package/00_perl_conf.sh
+++ package/00_perl_conf.sh
@@ -6,6 +6,7 @@ grep "ms" ./package/cpan_mirrors_test.txt | grep -w '\->' | awk '{print $3,$1}' 
 #grep "ms" ./package/cpan_mirrors_test.txt | grep -w '\->' | awk '{if($3<100){print $3,$1}}' | sort -V | cut -d" " -f2 | tr '\n' ' ' > ./package/cpan_mirrors_selected.txt
 
 # configure cpan
+# http://mirrors.163.com/cpan/
 echo -e "o conf urllist $(cat ./package/cpan_mirrors_selected.txt) \n o conf commit" | sudo cpan
 
 # upgrade all installed modules
