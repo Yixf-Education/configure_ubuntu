81beb0238081463116c1b32fc6d83e76611178f1
Yixf(Yi Xianfu) <yixfgit@gmail.com>
2016-09-24 10:49:02 +0800

    Correct script

--- utils/anaconda.sh
+++ utils/anaconda.sh
@@ -1,4 +1,4 @@
-bash /backup/Sources/wps/Anaconda2*.sh
-bash /backup/Sources/wps/Anaconda3*.sh
+bash /backup/Sources/Anaconda2*.sh
+bash /backup/Sources/Anaconda3*.sh
 
 # Add PATH to .bashrc and .zshrc
